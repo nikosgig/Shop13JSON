b4c647474435c1333a8ad8367bb2b2cba35e6f55
nikosgig <nikosgigelos@gmail.com>
2015-07-23 09:49:23 +0300

    hi

---
+++ app/src/main/java/info/androidhive/customlistviewvolley/MainActivity.java
@@ -0,0 +1,142 @@
+package info.androidhive.customlistviewvolley;
+
+import info.androidhive.customlistviewvolley.adater.CustomListAdapter;
+import info.androidhive.customlistviewvolley.app.AppController;
+import info.androidhive.customlistviewvolley.model.Product;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+public class MainActivity extends Activity {
+	// Log tag
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	// Products json url
+	private static final String url = "http://www.shop13.gr/app_search.php";
+
+	private ProgressDialog pDialog;
+	private List<Product> productList = new ArrayList<Product>();
+	private ListView listView;
+	private CustomListAdapter adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		listView = (ListView) findViewById(R.id.list);
+		adapter = new CustomListAdapter(this, productList);
+		listView.setAdapter(adapter);
+
+		pDialog = new ProgressDialog(this);
+		// Showing progress dialog before making http request
+		pDialog.setMessage("Loading...");
+		pDialog.show();
+
+		// changing action bar color
+		getActionBar().setBackgroundDrawable(
+				new ColorDrawable(Color.parseColor("#1b1b1b")));
+
+		// Creating volley request obj
+		JsonArrayRequest productReq = new JsonArrayRequest(url,
+				new Response.Listener<JSONArray>() {
+					@Override
+					public void onResponse(JSONArray response) {
+						Log.d(TAG, response.toString());
+						hidePDialog();
+
+						// Parsing json
+						for (int i = 0; i < response.length(); i++) {
+							try {
+
+								JSONObject obj = response.getJSONObject(i);
+								Product product = new Product();
+								product.setId(((Number) obj.get("id")).intValue());
+                                try {
+                                    product.setName(new String(obj.getString("name").getBytes("ISO-8859-7"), "UTF-8"));
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                }
+								product.setThumbnailUrl(obj.getString("img"));
+								//product.setRating(((Number) obj.get("rating")).doubleValue());
+								product.setPrice(obj.getString("price"));
+                                product.setSiteUrl(obj.getString("url"));
+                                product.setType(obj.getString("type"));
+
+								// Genre is json array
+								/*JSONArray genreArry = obj.getJSONArray("genre");
+								ArrayList<String> genre = new ArrayList<String>();
+								for (int j = 0; j < genreArry.length(); j++) {
+									genre.add((String) genreArry.get(j));
+								}
+								product.setGenre(genre);*/
+
+								// adding product to movies array
+								productList.add(product);
+
+							} catch (JSONException e) {
+								e.printStackTrace();
+							}
+
+						}
+
+						// notifying list adapter about data changes
+						// so that it renders the list view with updated data
+						adapter.notifyDataSetChanged();
+					}
+				}, new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						VolleyLog.d(TAG, "Error: " + error.getMessage());
+						hidePDialog();
+
+					}
+				});
+
+		// Adding request to request queue
+		AppController.getInstance().addToRequestQueue(productReq);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		hidePDialog();
+	}
+
+	private void hidePDialog() {
+		if (pDialog != null) {
+			pDialog.dismiss();
+			pDialog = null;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
